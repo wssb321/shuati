910163d703f87243e5a237e0b7a0362028cb3758
不是我 <3067805413@qq.com>
2026-05-16 15:54:43 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { QuizPage } from "@/pages/QuizPage";
 
 export default function App() {
